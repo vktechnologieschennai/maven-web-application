5748409ce94a4bbf897ea5543e77e0fea5569d5f
Venkat K <venkatdevopstraining@gmail.com>
2020-10-09 06:08:18 +0530

    Update EmployeeService.java

--- src/main/java/com/nt/services/EmployeeService.java
+++ src/main/java/com/nt/services/EmployeeService.java
@@ -23,7 +23,7 @@ public class EmployeeService {
 
 		JSONObject js = new JSONObject();
 		js.put("Name", "Vijaya Srinivas Arepalli");
-		js.put("Calling Name", "Sreeni");
+		js.put("Calling Name", "Sri");
 		js.put("DOB", "08-Nov-2011");
 		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
 		js.put("Places he like", "His native place");
